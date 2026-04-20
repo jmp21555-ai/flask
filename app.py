7616191ba63609f99e4a71de9e525bf3c3fd008b
jmp21555-ai <jmp21555@gmail.com>
2026-04-20 17:47:50 +0200

    Change default port from 5000 to 8080

--- app.py
+++ app.py
@@ -108,5 +108,5 @@ def test_binance():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
