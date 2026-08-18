3e317ea946320092b0339b99a1919b315e8619d9
jmp21555-ai <jmp21555@gmail.com>
2026-08-18 17:32:48 +0200

    Fix indentation in app.py

--- app.py
+++ app.py
@@ -152,4 +152,4 @@ def check_order(order_id):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port) 
